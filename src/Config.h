c477dca2d5eb59d09d53bf606d6d0112a564d86f
Night <davidjantho@gmail.com>
2024-05-23 22:08:17 +0200

    adding to config.h

--- src/Config.h
+++ src/Config.h
@@ -4,4 +4,7 @@ struct {
     const char* ssid = "SSID HERE";
     const char* password = "PASSWORD";
     const int port = 6900;
+    const IPAddress local_IP = IPAddress(192, 168, 0, 20);
+    const IPAddress gateway = IPAddress(192, 168, 0, 1);
+    const IPAddress subnet = IPAddress(255, 255, 255, 0);
 } WiFiSettings;
